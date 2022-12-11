3e16fca7e6c160a820e0114e524f633d14a2fef1
MartaAlet <72511288+MartaAlet@users.noreply.github.com>
2022-12-11 20:36:42 +0100

    g

--- Part_4/myapp/analytics/analytics_data.py
+++ Part_4/myapp/analytics/analytics_data.py
@@ -10,7 +10,7 @@ class AnalyticsData:
     # statistics table 1
     # fact_clicks is a dictionary with the click counters: key = doc id | value = click counter
     fact_clicks = dict([])
-
+    
     # statistics table 2 
     #fact_query is a dictionary with the search counters: key = query | value = search counter
     #fact_query_length is a list of the query lengths
@@ -18,9 +18,12 @@ class AnalyticsData:
     fact_query_len = list()
 
     # statistics table 3
-    #fact_query is a dictionary with the last time search: key = query | value = last time searched
+    #fact_query_time is a dictionary with the last time search: key = query | value = last time searched
     fact_query_time = dict([])
-
+    
+    # statistics table 3
+    #fact_doc_query is a dictionary with the queries related to a visited document: key = doc id | value = list of queries
+    fact_doc_query = dict([])
     def save_query_terms(self, terms: str) -> int:
         print(self)
         return random.randint(0, 100000)
